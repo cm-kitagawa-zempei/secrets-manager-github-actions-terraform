9f0bc2c9e65c4d02343b99632be0b22412a8d8ec
cm-kitagawa.zempei <kitagawa.zempei@classmethod.jp>
2025-12-06 17:02:03 +0900

    Remove thumbprint_list from aws_iam_openid_connect_provider

--- main.tf
+++ main.tf
@@ -17,9 +17,8 @@ provider "aws" {
 # OIDC Provider for GitHub Actions
 # ============================================
 resource "aws_iam_openid_connect_provider" "github_actions" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
 }
 
 # ============================================
